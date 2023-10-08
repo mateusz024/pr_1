263e93c035f51ad57265b9b8afeac5f88c5d363a
mateusz024 <lach.dell876@gmail.com>
2023-10-08 22:38:34 +0200

    pr-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:pr_1/screenHome/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Myapp());
 }
 
-class MyApp extends StatelessWidget {
+class Myapp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-  return MaterialApp(
-      home: Scaffold(
-  appBar: AppBar(
-  title: Text('Lista'),
-  ),
-  body: MyListView(),
-      ),
-  );
-  }
-}
-
-class MyListView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        final names = ['Janusz', 'Barbara', 'Major', 'Ewelina', 'Ryszard'];
-        final surnames = ['Zupa', 'Sałata', 'Zakrzewski', 'Kowalka', 'Pająk'];
-        final icons = [
-          Icons.bookmark,
-          Icons.ac_unit,
-          Icons.youtube_searched_for,
-          Icons.adb,
-          Icons.wc,
-        ];
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: Card(
-            child: ListTile(
-              leading: Icon(icons[index]),
-              title: Text('${names[index]} ${surnames[index]}'),
-            ),
-          ),
-        );
-
-      },
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
